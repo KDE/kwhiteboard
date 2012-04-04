f483678633750f278deb8da978c4ce7cf23eff8b
Daniele E. Domenichelli <daniele.domenichelli@gmail.com>
2012-04-04 17:18:16 +0200

    Clean main.cpp
     * Make KWhiteboard a KTp::TelepathyHandlerApplication
     * Remove signal handlers
     * Remove --initiate command line argument
     * Remove unused headers
     * Cleanup old commented stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 /*
- * From t-i-d main.cpp
- *
  * Copyright (C) 2009-2010 Collabora Ltd. <info@collabora.co.uk>
  *   @author George Goldberg <george.goldberg@collabora.co.uk>
  * Copyright (C) 2010 David Faure <faure@kde.org>
+ * Copyright (C) 2012 Daniele E. Domenichelli <daniele.domenichelli@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,88 +19,33 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-extern "C"
-{
-#include <signal.h>
-}
-
 #include "TelepathyChannelManager.h"
-#include "TubeLauncher.h"
 #include "kwhiteboard.h"
 
 #include <KAboutData>
 #include <KCmdLineArgs>
 #include <KDebug>
-#include <KApplication>
 
-#include <QtCore/QTimer>
-
-#include <TelepathyQt/Types>
 #include <TelepathyQt/Debug>
 
-namespace
-{
-    static void signal_handler(int signal)
-    {
-        if ((signal == SIGTERM) || (signal == SIGINT)) {
-            QCoreApplication * const app(QCoreApplication::instance());
-            if (app != 0) {
-                app->quit();
-            }
-        }
-    }
-}
+#include <KTp/telepathy-handler-application.h>
+
 
 int main(int argc, char *argv[])
 {
-    KAboutData aboutData("kwhiteboard", 0, ki18n("KWhiteBoard"),
-                         "1.0", ki18n("KDE WhiteBoard Application"), KAboutData::License_LGPL);
+    KAboutData aboutData("kwhiteboard", 0, ki18n("KWhiteboard"),
+                         "1.0", ki18n("KDE Whiteboard Application"), KAboutData::License_LGPL);
+    aboutData.addAuthor(ki18n("Daniele E. Domenichelli"), ki18n("Developer"), "daniele.domenichelli@gmail.com");
     KCmdLineArgs::init(argc, argv, &aboutData);
 
-    KCmdLineOptions options;
-    options.add("initiate", ki18n("initiate"), "1");
-
-    KCmdLineArgs::addCmdLineOptions(options);
-
-    KApplication app;
-
-    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
-
-    // Initialise Telepathy.
-    Tp::registerTypes();
-    Tp::enableDebug(true);
-    Tp::enableWarnings(true);
-
-    // Set up signal handlers.
-    if (signal(SIGINT, signal_handler) == SIG_ERR) {
-        kWarning() << "Setting up SIGINT signal handler failed.";
-    }
-
-    if (signal(SIGTERM, signal_handler) == SIG_ERR) {
-        kWarning() << "Setting up SIGTERM signal handler failed.";
-    }
+    KTp::TelepathyHandlerApplication app;
 
     TelepathyChannelManager *tpChannelManager = new TelepathyChannelManager;
     tpChannelManager->initialize();
 
-    // Set up the main widget
-    //MainWidget *mainWidget = new MainWidget;
-
-    //QObject::connect(mainWidget, SIGNAL(destroyed()),
-      //      &app, SLOT(quit()));
-
     KWhiteBoard *mainWindow = new KWhiteBoard();
     mainWindow->show();
 
-    if (args->isSet("initiate")) {
-
-        TubeLauncher *tubeLauncher = new TubeLauncher;
-        QObject::connect(tpChannelManager, SIGNAL(gotTubeChannel(Tp::ChannelPtr)),
-                            tubeLauncher, SLOT(onGotTubeChannel(Tp::ChannelPtr)));
-
-        QTimer::singleShot(10000, tubeLauncher, SLOT(onStartWhiteBoardSessionClicked()));
-
-    }
     QObject::connect(tpChannelManager, SIGNAL(gotTubeDBusConnection(QDBusConnection)),
                      mainWindow, SLOT(onGotTubeDBusConnection(QDBusConnection)));
 
